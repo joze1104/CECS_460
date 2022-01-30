a09b49711c14eb6b65108c1937797d0e8b6db270
joze1104 <89559626+joze1104@users.noreply.github.com>
2022-01-29 23:44:50 -0800

    progress on lab1

---
+++ Lab1/type.h
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------
+ * Copyright (c) 2009 Keil - An ARM Company. All rights reserved.
+ *----------------------------------------------------------------------------*/
+
+#ifndef __TYPE_H__
+#define __TYPE_H__
+
+#ifndef NULL
+#define NULL    ((void *)0)
+#endif
+
+#ifndef __FALSE
+#define __FALSE   (0)
+#endif
+
+#ifndef __TRUE
+#define __TRUE    (1)
+#endif
+
+typedef char               S8;
+typedef unsigned char      U8;
+typedef short              S16;
+typedef unsigned short     U16;
+typedef int                S32;
+typedef unsigned int       U32;
+typedef long long          S64;
+typedef unsigned long long U64;
+typedef unsigned char      BIT;
+typedef unsigned int       BOOL;
+
+#endif  /* __TYPE_H__ */
